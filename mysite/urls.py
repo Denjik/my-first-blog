0f445e97087f3a02a92b30c25b2a484e4b103701
Denjik <webdesing44@ukr.net>
2019-02-08 16:18:21 +0200

    Added view and template for detailed blog post as well as CSS for the site.

--- mysite/urls.py
+++ mysite/urls.py
@@ -20,7 +20,7 @@ from django.contrib.auth import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-	re_path(r'accounts/login/', views.LoginView.as_view(), name='login'),
-    re_path(r'accounts/logout/', views.LogoutView.as_view(next_page='/'), name='logout'),
+	url(r'accounts/login/', views.LoginView.as_view(), name='login'),
+    url(r'accounts/logout/', views.LogoutView.as_view(next_page='/'), name='logout'),
 	url(r'', include('blog.urls')),
 ]
